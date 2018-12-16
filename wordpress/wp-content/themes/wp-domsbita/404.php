4dd91c6292bad1e72910070a536a5e6381d1a4e4
Vitalii Antoniuk <va@jelasric.com>
2018-12-16 13:38:00 +0200

    close and archive project

--- wordpress/wp-content/themes/wp-domsbita/404.php
+++
@@ -1,8 +0,0 @@
-<?php get_header(); ?>
-
-  <div class="content">
-    <h2 class="page-headding"><?php _e( 'Page not found', 'wpeasy' ); ?></h2>
-    <h2><a href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'wpeasy' ); ?></a></h2>
-  </div>
-
-<?php get_footer(); ?>
